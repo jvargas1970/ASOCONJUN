509250115bfae1685828d5b015e9c2219d412de4
Javier Vargas <jvargas@sercoaconta.com>
2026-09-15 11:11:49 -0600

    Configurar static y fondo en index.html

--- asoconjun/settings.py
+++ asoconjun/settings.py
@@ -132,6 +132,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
